7f2507610a5b926778d7584644aa76349f9929a9
nathangauth88 <nathangauth@gmail.com>
2022-08-02 20:25:07 -0700

    Deploy to Heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Configure
 require('dotenv').config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || "4005";
 
 //Connection
 const MONGO_URL = process.env.MONGO_URL;
